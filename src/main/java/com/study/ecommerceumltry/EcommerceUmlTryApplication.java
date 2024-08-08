5cdfac1e90cefb5f1a6105a333232e6e67296c8c
Wes <wal2.designer@gmail.com>
2024-08-08 11:51:50 +0100

    Initial commit

---
+++ src/main/java/com/study/ecommerceumltry/EcommerceUmlTryApplication.java
@@ -0,0 +1,13 @@
+package com.study.ecommerceumltry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUmlTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceUmlTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/ecommerceumltry/EcommerceUmlTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ecommerceumltry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUmlTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
